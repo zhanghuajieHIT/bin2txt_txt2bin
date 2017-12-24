9fe8e39e6218b580a9b10ce2358fed9d70c0e8bd
zhanghuajie <310336422@qq.com>
2017-12-24 20:52:47 +0800

    二进制文件和文本文件间的转换

---
+++ include/change.h
@@ -0,0 +1,6 @@
+#ifndef CHANGE_FORMAT_H
+#define CHANGE_FORMAT_H
+//定义转化需要的类以及声明函数
+
+
+#endif //CHANGE_FORMAT_H
---
+++ source/main.cpp
@@ -0,0 +1,10 @@
+/*实现二进制文件和文本文件的相互转换
+  by zhanghuajie 20171224
+*/
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    return 0;
+}
